c44757b50d9e3eeacf3da99cdb13d374c7453db3
jkgyso <jkgyso@gmail.com>
2024-07-16 20:48:51 +0800

    changed text animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
-import Typical from 'react-typical';
 import heroGif from '../images/hero.gif';
 import Features from '../components/Features';
+import '../App.css'; // Import your CSS file
 
 export default function Home() {
   return (
     <>
       <Row className="align-items-center">
         <Col md={6} className="p-4 text-center">
-          <h1>
-            <Typical
-              steps={['Captivate', 2000, 'Communicate', 2000, 'Contribute', 4000, 'Inspire!', 4000]}
-              loop={Infinity}
-              wrapper="span"
-              className="typical-text"
-            />
+          <h1 className="animate-text">
+            Captivate, Communicate, Contribute, Inspire!
           </h1>
           <p>Join us as we explore, engage, and enrich together.</p>
-          <Link className="btn" style={{ backgroundColor: '#f3d8c7', color: 'black' }} to={'/blogs/getBlogs'}>
+          <Link className="btn" style={{ backgroundColor: '#f3d8c7', color: 'black' }} to="/blogs/getBlogs">
             <strong>Start reading</strong>
           </Link>
         </Col>
